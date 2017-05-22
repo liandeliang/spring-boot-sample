95bc6e76ec33f868d94af6f21e5ca7e4c29d6705
liandeliang <liandeliang@gmail.com>
2017-05-22 16:16:00 +0800

    fixed gateway call a b

--- pkrss-microsrv-gateway/src/main/java/services/Application.java
+++ pkrss-microsrv-gateway/src/main/java/services/Application.java
@@ -2,14 +2,10 @@ package services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 
 @SpringBootApplication
-@EnableHystrix
 @EnableSidecar
-@EnableEurekaClient
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
